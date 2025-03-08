afbb58b76a24b385ff0246a781c66665df26ca6f
LucasMezza <mezza.lucas12@gmail.com>
2025-03-08 13:17:25 -0300

    book a ticket post

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,10 +1,49 @@
 import { useEffect, useState } from 'react';
 import { NavLink, useParams } from 'react-router-dom'
-import { getEventById } from '../services/crud';
+import { addBook, getEventById } from '../services/crud';
 
 export const EventDetail = () => {
   const { id } = useParams();
   const [ event, setEvent ] = useState(null)
+  const [ formShow, setFormShow ] = useState(false)
+  const [ totalPrice, setTotalPrice ] = useState(0)
+  const [ state, setState ] = useState('')
+
+  const showFrom = e => {
+    setFormShow(prevState => !prevState)
+  }
+
+  const handleTotal = e => {
+    const total = event.price * Number(e.target.value)
+    setTotalPrice(total)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    if(!formData.get('customerName')) {
+      setState('Fill in all the fields')
+      return
+    }
+
+    if(!formData.get('ticketQuantity') || Number(formData.get('ticketQuantity')) < 1 || Number(formData.get('ticketQuantity')) > 10 ) {
+      setState('Ticket quantity have to be between 1 - 10')
+      return
+    }
+
+    const body = {
+      eventId: event.id,
+      ticketQuantity: Number(formData.get('ticketQuantity')),
+      customerName: formData.get('customerName')
+    }
+
+    addBook(body)
+      .then(res => setState("Book a ticket successfully"))
+      .catch(err => setState("Server error"))
+  }
 
   useEffect(() => {
     getEventById(id)
@@ -32,6 +71,28 @@ export const EventDetail = () => {
                 <p>Event escription: {event.descripction}</p>
                 <span>Ticket price: ${event.price}</span>
                 <span>Available tickets: {event.availableTickets}</span>
+                {formShow ? 
+                  <form onSubmit={handleSubmit}>
+                    <div>
+                      <label>Customer´s name</label>
+                      <input type="text" name='customerName' />
+                    </div>
+                    <div>
+                      <label>Tickets to book</label>
+                      <input type="number" min={1} max={10} name='ticketQuantity' onInput={handleTotal}/>
+                    </div>
+                    <div>
+                      <label>Total price: ${totalPrice}</label>
+                    </div>
+                    {state ? <p style={{color: "red"}}>{state}</p> : null}
+                    <div>
+                      <button disabled={totalPrice == 0}>Book now!</button>
+                      <button onClick={showFrom}>Cancel</button>
+                    </div>
+                  </form>
+                  : null
+                }
+                {!formShow ?  <button onClick={showFrom}>Book now!</button> : null}
               </article>
           </>
           : <p>Event not found</p>
--- frontend/src/services/crud.js
+++ frontend/src/services/crud.js
@@ -7,14 +7,28 @@ export const getEvents = () => {
     .catch(err => {throw new Error(err)})
   )  
 }
-
-
 export const getEventById = (id) => {
   return (
     fetch(`https://goldfish-app-fbulw.ondigitalocean.app/event/${id}?applicationId=d5b22b06-a188-4f7d-87fd-3632551bde15`, {
       method: "GET",
     })
     .then(res => res.json())
+    .catch(err => {throw new Error(err)})
+  )  
+}
+
+export const addBook = (body) => {
+  const bodyObj = {...body, applicationId: "d5b22b06-a188-4f7d-87fd-3632551bde15", userId: "3d76e5b8-2ae3-4a29-bb94-0d519fd5f206"}
+
+  return (
+    fetch(`https://goldfish-app-fbulw.ondigitalocean.app/booking`, {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(bodyObj)
+    })
+    .then(res => res)
     .catch(err => {throw new Error(err)})
   )  
 }
