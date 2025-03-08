23142685d42299befd718fe95a273d3a32c9debd
LucasMezza <mezza.lucas12@gmail.com>
2025-03-08 11:53:30 -0300

    get users tickets get not finish yet

--- backend/src/routes/crud.js
+++ backend/src/routes/crud.js
@@ -41,7 +41,7 @@ router.get("/events", async (req, res) => {
 
     events.sort((a,b) => new Date(a.date).getTime() - new Date(b.date).getTime())
 
-    const totalDocuments = await Event.countDocuments({date : { $gt: "2025-03-10" }})
+    const totalDocuments = await Event.countDocuments({date : { $gt: currentDate }})
     const totalPages = Math.ceil(totalDocuments / pageSize);
     res.status(201).json({
       currentPage: page,
@@ -119,6 +119,66 @@ router.delete("/tickets/:ticketId", async (req, res) => {
 
     await eventExist.save();
     res.status(204).json({ message: "Ticket deleted successfully" });
+  } catch {
+    res.status(500).json({ error: "Server error"})
+  }
+})
+
+router.get("/users/:userId/tickets", async (req, res) => {
+  const { userId } = req.params
+  const page = Number(req.query.page) || 1;
+  const pageSize = Number(req.query.pageSize) || 10;
+  const startDate = req.query.startDate || '2000-01-10';
+  const endDate = req.query.endDate || Date.now();
+  const sort = Boolean(req.query.sort) || false;
+  const search = String(req.query.search) || '';
+
+  const skipEvents = (page - 1) * pageSize;
+
+  try {
+    const tickets = await Ticket.find({userId: userId}).skip(skipEvents).limit(pageSize)  
+    const eventsIds = tickets.map(row => row.eventId)  
+
+    const events = await Event.find({ 
+      userId: eventsIds,
+      date : { 
+        $gt: startDate,
+        $lte: endDate
+      }, 
+      name: search 
+      }).skip(skipEvents).limit(pageSize)
+
+    console.log(events)
+    if(sort) {
+      events.sort((a,b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    }
+
+    const ticketdId = tickets.reduce((acc, elm) => {
+      if(!acc.hasOwnProperty(elm.eventId)) {
+        acc[elm.eventId] = elm;
+      }
+
+      return acc
+    })
+
+    const eventAndTickets = events.map(row => {
+      const ticket = ticketdId[row._id]
+      return {
+        ticketId: ticket._id,
+        userId: ticket.userId,
+        eventId: ticket.eventId,
+        name: row.name,
+        date: row.date
+      }
+    })
+
+    const totalDocuments = await Event.countDocuments({date : { $gt: "2025-03-10" }})
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+    res.status(201).json({
+      currentPage: page,
+      pageSize: pageSize,
+      totalPages: totalPages,
+      events: eventAndTickets})
   } catch {
     res.status(500).json({ error: "Server error"})
   }
